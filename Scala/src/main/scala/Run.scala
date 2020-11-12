cf62d330b493b4a0a58ee8d1af1534861498a885
mzacha02 <mzacha02@dcs.bbk.ac.uk>
2020-11-12 11:14:52 +0000

    delete

--- Scala/src/main/scala/Run.scala
+++
@@ -1,7 +0,0 @@
-object Run {
-  var t = new Test("Hello World")
-
-  def main(args: Array[String]): Unit = {
-    println(t.Print)
-  }
-}
--- Scala/src/main/scala/Test.scala
+++
@@ -1,4 +0,0 @@
-class Test (s: String){
-  def Print = s
-}
-
